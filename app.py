1a777b0d61bb2ba1396a535dfe0461758158191c
andersonkyris-commits <andersonkyris@gmail.com>
2026-06-09 18:08:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -212,14 +212,6 @@ if sport == "Football ⚽":
     compet_choisie = st.selectbox("Compétition", list(DICT_COMPETS.keys()))
     code_compet = DICT_COMPETS[compet_choisie]
      
-    try:
-        FOOTBALL_API_KEY = st.secrets["FOOTBALL_API_KEY"]
-        st.write("Clé Football détectée")
-    except Exception as e:
-        st.error(f"Clé Football absente : {e}")
-    
-    st.write("Football headers existe :", "football_headers" in globals())
-
     @st.cache_data(ttl=1800)
     def charger_matchs(code, headers):
         if code == "MANUAL":
